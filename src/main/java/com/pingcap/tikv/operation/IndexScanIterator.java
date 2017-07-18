0eca5163e539e259748c193b92df74414e2809f0
zhexuany <yangzhexuan@pingcap.com>
2017-07-18 16:45:14 +0800

    finish double read

--- src/main/java/com/pingcap/tikv/operation/IndexScanIterator.java
+++ src/main/java/com/pingcap/tikv/operation/IndexScanIterator.java
@@ -20,10 +20,14 @@ import com.google.protobuf.ByteString;
 import com.pingcap.tikv.Snapshot;
 import com.pingcap.tikv.codec.KeyUtils;
 import com.pingcap.tikv.codec.TableCodec;
+import com.pingcap.tikv.grpc.Coprocessor;
 import com.pingcap.tikv.grpc.Coprocessor.KeyRange;
 import com.pingcap.tikv.meta.TiSelectRequest;
 import com.pingcap.tikv.row.Row;
-import java.util.Iterator;
+import com.pingcap.tikv.util.Comparables;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 // A very bad implementation of Index Scanner barely made work
 // TODO: need to make it parallel and group indexes
@@ -46,14 +50,47 @@ public class IndexScanIterator implements Iterator<Row> {
     return iter.hasNext();
   }
 
+  private List<KeyRange> mergeKeyRangeList(List<KeyRange> keyRangeList) {
+    // sort key ranges according to its start key in order to do further merge.
+    List<KeyRange> newKeyRanges = new LinkedList<>();
+    keyRangeList.sort((a, b) -> Comparables.wrap(a.getStart()).compareTo(b.getStart()));
+    for(int i = 0; i < keyRangeList.size() - 1; i++) {
+        KeyRange a = keyRangeList.get(i);
+        KeyRange b = keyRangeList.get(i + 1);
+        // since keyRangeList is already sorted. when a's end is larger or equal to b's start
+        // a and b are two keys can be merged since a's start is always smaller or equal to b's start.
+        // e.g.
+        // [1, 2) and [2, 3)
+        // [1, 4) and [2, 3)
+        if(Comparables.wrap(a.getEnd()).compareTo(b.getStart()) >= 0) {
+          ByteString largerEnd;
+          if (Comparables.wrap(a.getEnd()).compareTo(b.getEnd()) >= 0) {
+            largerEnd = a.getEnd();
+          } else {
+            largerEnd = b.getEnd();
+          }
+          newKeyRanges.add(KeyRange.newBuilder().setStart(a.getStart()).setEnd(largerEnd).build());
+        }
+    }
+    return newKeyRanges;
+  }
+
   private Iterator<Row> doubleRead() {
-    // this is handles
-    Row r = iter.next();
-    long handle = r.getLong(0);
-    ByteString startKey = TableCodec.encodeRowKeyWithHandle(selReq.getTableInfo().getId(), handle);
-    ByteString endKey = ByteString.copyFrom(KeyUtils.prefixNext(startKey.toByteArray()));
-    selReq.resetRanges(
-        ImmutableList.of(KeyRange.newBuilder().setStart(startKey).setEnd(endKey).build()));
+    // first read of double read's result are handles.
+    // This is actually keys of our second read.
+    List<KeyRange> keyRangeList = new LinkedList<>();
+    while(iter.hasNext()) {
+      Row r = iter.next();
+      long handle = r.getLong(0);
+      // in order to improve the speed of second read, we merge key if necessary.
+      // [1, 3), [3, 4) will be [1, 4) after merge
+      // [1, 2), [3, 4) will remain as same since they do not share any intersection.
+      ByteString startKey = TableCodec.encodeRowKeyWithHandle(selReq.getTableInfo().getId(), handle);
+      ByteString endKey = ByteString.copyFrom(KeyUtils.prefixNext(startKey.toByteArray()));
+      keyRangeList.add(KeyRange.newBuilder().setStart(startKey).setEnd(endKey).build());
+    }
+
+    selReq.resetRanges(mergeKeyRangeList(keyRangeList));
     return snapshot.select(selReq);
   }
 
