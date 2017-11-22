852d0e2f246b6589aa44bc1157eb468b7d9f8a4f
Ma Xiaoyu <maxiaoyu@pingcap.com>
2017-11-23 01:29:00 +0800

    a travial fix before DAG

--- src/main/java/com/pingcap/tikv/operation/SchemaInfer.java
+++ src/main/java/com/pingcap/tikv/operation/SchemaInfer.java
@@ -75,8 +75,13 @@ public class SchemaInfer {
 
     // append aggregates if present
     if (!tiSelectRequest.getAggregatePairs().isEmpty()) {
-      for (Pair<TiExpr, DataType> pair : tiSelectRequest.getAggregatePairs()) {
-        rowTrans.addProjection(new Cast(pair.second));
+      for (int i = 0; i < tiSelectRequest.getAggregatePairs().size(); i++) {
+        if (!tiSelectRequest.isAggregatesSkipped(i)) {
+          Pair<TiExpr, DataType> pair = tiSelectRequest.getAggregatePairs().get(i);
+          rowTrans.addProjection(new Cast(pair.second));
+        } else {
+          rowTrans.addProjection(Skip.SKIP_OP);
+        }
       }
     } else {
       for (TiExpr field : tiSelectRequest.getFields()) {
@@ -99,8 +104,13 @@ public class SchemaInfer {
       types.add(DataTypeFactory.of(TYPE_BLOB));
       tiSelectRequest.getAggregates().forEach(expr -> types.add(expr.getType()));
     } else {
-      // Extract all column type information from TiExpr
-      tiSelectRequest.getFields().forEach(expr -> types.add(expr.getType()));
+      // skip the only column if no aggregates but group by exists
+      // those cases are distinct and group by without aggregates
+      // and we append fake column for it
+      if (tiSelectRequest.getGroupByItems().isEmpty()) {
+        // Extract all column type information from TiExpr
+        tiSelectRequest.getFields().forEach(expr -> types.add(expr.getType()));
+      }
     }
   }
 
